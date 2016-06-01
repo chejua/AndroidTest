a5036ec346d2ac3c2853f3856e78bf03e519b9c4
Geo <ggaleano2014@fau.edu>
2016-05-31 21:16:15 -0400

    Added a comment to main to see if it works

--- app/src/main/java/com/example/geo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/geo/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -83,6 +84,12 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView)findViewById(R.id.imageView);
 
         //comment para ver si funciono
+
+        String code = "";
+
+        int color;
+
+        RelativeLayout ewc;
 
     }
 }
